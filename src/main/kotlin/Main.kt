24a7cbab1bd8348a1b06cbe30e638cd79918ac95
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-03 07:43:20 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,57 @@
+fun main () {
+   var num1 = 240
+   var num2 = 45
+   var addition =num1 + num2
+   println(addition)
+   println(num1 + num2/2)
+
+
+   var division = addition /2
+   println(division)
+
+   var name = "janet"
+   var a = 3
+   var b = 2
+   println(a)
+   a = a + b
+   println (a)
+
+
+   a = a - b
+   println (a)
+   a = a * b
+   println(a)
+
+//increment
+   var age = 23
+   age++
+   println(age)
+
+
+   println(++age)
+
+   println(a<b)
+   println(b>a)
+   println(b<a)
+   println(a>b)
+
+   println(2==b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
